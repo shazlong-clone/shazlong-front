3b3f2411a17302133096a0ad841b967f4bd00053
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-06-05 20:35:28 +0300

    add

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { HiUserGroup } from 'react-icons/hi';
+import { BiVideoPlus } from 'react-icons/bi';
+import { MdSupportAgent } from 'react-icons/md';
+import { RiPsychotherapyLine } from 'react-icons/ri';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 function FooterMenu() {
-    return (
-        <div className='bg-white fixed w-full bottom-0 right-0 text-gray p-8'>
-            <div>ss</div>
-        </div>
-    );
+  const footerMenu = [
+    { id: 1, title: 'Therapists', icon: <HiUserGroup /> },
+    { id: 2, title: 'Online', icon: <BiVideoPlus /> },
+    { id: 3, title: 'My Therapy', icon: <RiPsychotherapyLine /> },
+    { id: 4, title: 'Support', icon: <MdSupportAgent /> },
+    { id: 5, title: 'More', icon: <FiMoreHorizontal /> }
+  ];
+  return (
+    <div className='bg-white fixed w-full bottom-0 right-0 text-gray p-2'>
+      <article className='flex gap-4 text-center justify-between'>
+        {footerMenu?.map((el) => {
+          return (
+            <section key={el?.id}>
+              <i className='text-xl'>{el.icon}</i>
+              <div className='text-sm'>{el?.title}</div>
+            </section>
+          );
+        })}
+      </article>
+    </div>
+  );
 }
 
 export default FooterMenu;
