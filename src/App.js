73707140f6a64cc2fb9ce0225ab008b436f1d9fd
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-07-19 18:12:37 +0300

    sovle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import NavBar from './components/Shared/NavBar';
 import FooterMenu from './components/Shared/FooterMenu';
 import { Loader } from 'rsuite';
@@ -19,25 +19,34 @@ const Test = lazy(() => import('./components/Shared/Test'));
 const Loading = () => {
   return <Loader size="lg" backdrop content="loading..." vertical />;
 };
+const LayOut = () => {
+  return (
+    <>
+      <div className="pb-[74px] md:pb-0">
+        <NavBar />
+        <Outlet />
+        <FooterMenu />
+      </div>
+    </>
+  );
+};
 function App() {
   return (
     <Suspense fallback={<Loading />}>
       <ErrorBoundary>
-        <div className="pb-[74px] md:pb-0">
-          <NavBar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/therapists" element={<Therapists />} />
-            <Route exact path="/thearpist-profile/:id" element={<ThearpistProfile />} />
-            <Route exact path="/checkout/:doctorId/:slotId" element={<Checkout />} />
-            <Route exact path="/instructions" element={<Instructions />} />
-            <Route exact path="/online" element={<Online />} />
-            <Route exact path="/my-therapy" element={<MyTherapy />} />
-            <Route exact path="/test" element={<Test />} />
+        <Routes>
+          <Route path="/" element={<LayOut />}>
+            <Route index element={<Home />} />
+            <Route exact path="therapists" element={<Therapists />} />
+            <Route exact path="thearpist-profile/:id" element={<ThearpistProfile />} />
+            <Route exact path="checkout/:doctorId/:slotId" element={<Checkout />} />
+            <Route exact path="instructions" element={<Instructions />} />
+            <Route exact path="online" element={<Online />} />
+            <Route exact path="my-therapy" element={<MyTherapy />} />
+            <Route exact path="test" element={<Test />} />
             <Route exact path="*" element={<Error />} />
-          </Routes>
-          <FooterMenu />
-        </div>
+          </Route>
+        </Routes>
       </ErrorBoundary>
     </Suspense>
   );
