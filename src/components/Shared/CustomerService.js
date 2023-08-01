ff3fb79f0f068f9c153dceeb0689d8e2749aefe7
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-08-01 14:56:03 +0300

    add cutomer serveice

--- src/components/Shared/CustomerService.js
+++ src/components/Shared/CustomerService.js
@@ -1,26 +1,78 @@
 import React, { useState } from 'react';
-import { IconButton } from 'rsuite';
-import { MdSupportAgent } from 'react-icons/md';
+import { Avatar, AvatarGroup, Badge } from 'rsuite';
+import { IoMdSend } from 'react-icons/io';
+import { AiFillHome } from 'react-icons/ai';
+import { BiCross, BiMessageAltDetail } from 'react-icons/bi';
+import Card from './Card';
+import { CgClose } from 'react-icons/cg';
+import logo_white from '../../assets/images/logo_white.png';
+import cs1 from '../../assets/images/cs1.jpg';
+import cs2 from '../../assets/images/cs2.png';
+import cs3 from '../../assets/images/cs3.jpg';
 import clsx from 'clsx';
-
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { RxCross2 } from 'react-icons/rx';
 function CustomerService() {
-  const [open, setOpen] = useState(false);
+  const [activeTabe, setActiveTabe] = useState(1);
   return (
-    <div className="fixed bottom-[100px] right-[100px]">
-      <IconButton onClick={() => setOpen(!open)} icon={<MdSupportAgent className="text-[45px]" />} appearance="primary" circle />
-      <section
-        className={clsx(
-          'absolute w-[300px] bottom-[60px] right-[60px]  bg-gray text-white scale-0 origin-bottom-right',
-          open ? 'animate-openchat' : 'animate-closechat',
-        )}
-      >
-        <p>tjis is bg</p>
-        <p>tjis is bg</p>
-        <p>tjis is bg</p>
-        <p>tjis is bg</p>
-        <p>tjis is bg</p>
-        <p>tjis is bg</p>
-      </section>
+    <div className="fixed top-0 left-0 h-[100vh] w-full bg-white z-[51]">
+      <aside className={clsx(activeTabe === 1) ? '' : 'hidden'}>
+        <section className="text-white bg-cyan px-5 p-10">
+          <article className="flex justify-between items-center">
+            <img width="50px" height="50px" src={logo_white} alt="intercomcdn" />
+            <CgClose />
+          </article>
+          <article className="mt-10 mb-4">
+            <AvatarGroup stack>
+              <Avatar circle key={cs1} src={cs1} alt="cs1" />
+              <Avatar circle key={cs2} src={cs2} alt="cs2" />
+              <Avatar circle key={cs3} src={cs3} alt="cs3" />
+            </AvatarGroup>
+          </article>
+          <article>
+            <h4 className="text-white/60">Hi Saeed !</h4>
+            <h4>How We Can Help You</h4>
+          </article>
+        </section>
+        <section className="px-5 pt-1 pb-5 bg-gradient-to-b from-cyan to-white">
+          <Card className="text-gray rounded-[10px] py-4">
+            <aside className="flex justify-between items-center">
+              <div>
+                <strong className="text-[14px]">Send Us a message</strong>
+                <p className="text-gray/60 text-[14px]">We typically reply in under a minute</p>
+              </div>
+              <div className="text-cyan flex items-center">
+                <IoMdSend />
+              </div>
+            </aside>
+          </Card>
+        </section>
+        <section className="absolute bottom-0 py-6 shadow-[#00000024_0px_0px_25px] w-full border-t border-b-0 border-r-0 border-l-0  border-solid border-gray/5 flex justify-evenly text-3xl">
+          <aside className="grid text-center text-cyan" onClick={() => setActiveTabe(1)}>
+            <span className=" flex justify-center h-[30px]">
+              <AiFillHome />
+            </span>
+            <strong className="text-sm">Home</strong>
+          </aside>
+          <aside className="grid text-center" onClick={() => setActiveTabe(2)}>
+            <span className="flex justify-center h-[30px]">
+              <Badge content={1}>
+                <BiMessageAltDetail />
+              </Badge>
+            </span>
+            <strong className="text-sm">messages</strong>
+          </aside>
+        </section>
+      </aside>
+      <aside className={clsx(activeTabe === 2 ? '' : 'hidden')}>
+        <section className="bg-cyan px-5 text-white">
+          <article className="flex items-center justify-between">
+            <MdArrowBackIosNew />
+            <h4>Customer Suport</h4>
+            <RxCross2 />
+          </article>
+        </section>
+      </aside>
     </div>
   );
 }
