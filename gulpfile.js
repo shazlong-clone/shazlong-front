884d5e8ea0f74ffd059aeab17401f037a887f7d7
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-08-15 14:17:33 +0300

    sovle gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ var autoprefixer = require('gulp-autoprefixer');
 var rtlcss = require('gulp-rtlcss');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var cssimport = require('gulp-cssimport');
+
 
 gulp.task('styles', function () {
-    return gulp.src(['./src/assets/css/main.css'])
+    return gulp.src(['./src/index.css'])
+        .pipe(cssimport({
+            filter: /^http:\/\//gi // exclude only http urls
+        }))
 		.pipe(sourcemaps.init())
         .pipe(autoprefixer(["last 2 versions", "> 1%"])) // Other post-processing.
-        .pipe(gulp.dest('dist')) // Output LTR stylesheets.
+        .pipe(gulp.dest('./src/assets/css')) // Output LTR stylesheets.
         .pipe(rtlcss()) // Convert to RTL.
         .pipe(rename({ suffix: '-rtl' })) // Append "-rtl" to the filename.
-		.pipe(sourcemaps.write('dist')) // Output source maps.
-        .pipe(gulp.dest('dist')); // Output RTL stylesheets.
+		.pipe(sourcemaps.write('./src/assets/css')) // Output source maps.
+        .pipe(gulp.dest('./src/assets/css')); // Output RTL stylesheets.
 });
