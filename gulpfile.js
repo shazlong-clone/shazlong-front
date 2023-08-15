1e987507825b79abfb34bfac4aefcd8112137684
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-08-15 16:49:53 +0300

    add gulp clearn css

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,21 @@ var rtlcss = require('gulp-rtlcss');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var cssimport = require('gulp-cssimport');
+const cleanCSS = require('gulp-clean-css');
 
 
 gulp.task('styles', function () {
     return gulp.src(['./src/index.css'])
         .pipe(cssimport({
-            filter: /^http:\/\//gi // exclude only http urls
+            filter: /^\.\/.+/gi // include only starts with ./ not http
         }))
 		.pipe(sourcemaps.init())
-        .pipe(autoprefixer(["last 2 versions", "> 1%"])) // Other post-processing.
+        .pipe(autoprefixer(['last 2 versions', '> 1%'])) // Other post-processing.
+        .pipe(rename('main.css')) // Append "-rtl" to the filename.
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./src/assets/css')) // Output LTR stylesheets.
         .pipe(rtlcss()) // Convert to RTL.
         .pipe(rename({ suffix: '-rtl' })) // Append "-rtl" to the filename.
-		.pipe(sourcemaps.write('./src/assets/css')) // Output source maps.
         .pipe(gulp.dest('./src/assets/css')); // Output RTL stylesheets.
+		// .pipe(sourcemaps.write('./src/assets/css')) // Output source maps.
 });
