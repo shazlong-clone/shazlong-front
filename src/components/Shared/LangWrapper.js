8162984c8b8924eb7187c7219c33f8ed819b6c7f
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-08-19 20:47:46 +0300

    sovle

--- src/components/Shared/LangWrapper.js
+++ src/components/Shared/LangWrapper.js
@@ -6,7 +6,7 @@ import { changeLang } from '../../features/theme/themeSlice';
 import { useThemeSwitcher } from 'react-css-theme-switcher';
 
 function LangWrapper({ children }) {
-  const { switcher, themes, status } = useThemeSwitcher();
+  const { switcher, themes } = useThemeSwitcher();
   const { i18n } = useTranslation();
   const currLang = i18n.resolvedLanguage;
   const dispatch = useDispatch();
@@ -18,6 +18,7 @@ function LangWrapper({ children }) {
     dispatch(changeLang(currLang));
 
     toggleDarkMode();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currLang]);
 
   return <>{children}</>;
