0cd9c86142a94aa92bd23b4bcf0dedcb90c6b57f
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-09-08 10:51:49 +0300

    add more translation

--- src/pages/JoinUs.js
+++ src/pages/JoinUs.js
@@ -13,74 +13,84 @@ import CalendarIcon from '@rsuite/icons/Calendar';
 import UserBadgeIcon from '@rsuite/icons/UserBadge';
 import TextImageIcon from '@rsuite/icons/TextImage';
 import { GiClick } from 'react-icons/gi';
-const data = [
-  {
-    image_path: location,
-    header: 'Online Sessions',
-    body: ' Availability to work from anywhere saving time, effort and cost. ',
-  },
-  {
-    image_path: earth,
-    header: 'Universal Therapy',
-    body: 'We serve clients from all over the world and you will get paid for both.',
-  },
-  {
-    image_path: chat,
-    header: 'Better Communication',
-    body: ' Availability to communicate with customers via a text chat or via a video call. ',
-  },
-  {
-    image_path: folder,
-    header: 'Client history',
-    body: ' Access all your client previous prescriptions, so you can give them better help ',
-  },
-  {
-    image_path: lock,
-    header: 'Private and Safe',
-    body: ' Shezlong is 100% private and secured. ',
-  },
-  {
-    image_path: note,
-    header: 'Recorded Notes',
-    body: ' Securely keep notes and medical reports for your clients. ',
-  },
-];
+import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
+
 function JoinUs() {
+  const { t, i18n } = useTranslation();
+
+  const data = [
+    {
+      image_path: location,
+      header: t('Online_Sessions'),
+      body: t('Online_Sessions_Body'),
+    },
+    {
+      image_path: earth,
+      header: t('Universal_Therapy'),
+      body: t('Universal_Therapy_Body'),
+    },
+    {
+      image_path: chat,
+      header: t('Better_Communication'),
+      body: t('Better_Communication_Body'),
+    },
+    {
+      image_path: folder,
+      header: 'Client history',
+      body: ' Access all your client previous prescriptions, so you can give them better help ',
+    },
+    {
+      image_path: lock,
+      header: 'Private and Safe',
+      body: ' Shezlong is 100% private and secured. ',
+    },
+    {
+      image_path: note,
+      header: 'Recorded Notes',
+      body: ' Securely keep notes and medical reports for your clients. ',
+    },
+  ];
+
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
   return (
     <>
       <div className="container">
-        <InternalHeader className="mb-5">Join Us</InternalHeader>
+        <InternalHeader className="mb-5">{t('Join_Us')}</InternalHeader>
       </div>
       <div
         className="bg-no-repeat bg-right bg-cover"
         style={{
-          backgroundImage: 'url(/img/join-cover.png)',
+          backgroundImage: `url(/img/join-cover${i18n.resolvedLanguage === 'ar' ? '-ar' : ''}.png)`,
         }}
       >
-        <section className="h-[460px] bg-gradient-to-r from-cyan to-transparent text-white px-5 lg:px-16">
-          <h3 className="pt-[50px]  lg:max-w-xl lg:text-4xl lg:mb-10">Join Shezlong now and make your own contribution</h3>
+        <section
+          className={clsx(
+            'h-[460px] bg-gradient-to-r from-cyan to-transparent  text-white px-5 lg:px-16',
+            i18n.resolvedLanguage === 'ar' ? 'bg-gradient-to-l' : 'bg-gradient-to-r',
+          )}
+        >
+          <h3 className="pt-[50px]  lg:max-w-xl lg:text-4xl lg:mb-10">{t('Join_Shezlong_now')}</h3>
           <p className="text-gray/70 lg:leading-9 text-[16px] mb-12 lg:max-w-xl lg:text-xl lg:mb-15">
-            Enjoy practicing psychotherapy online; now you can communicate with thousands of customers from all over the world
-            privately. Join us with other psychiatrists and psychologists worldwide.
+            {t('Join_Shzlong_now_body')}
           </p>
           <Link to="/doctor-signup">
-            <Button className="rounded-full font-bold">Join Us As Therapist</Button>
+            <Button className="rounded-full font-bold">{t('Join_As_A_Therapist')}</Button>
           </Link>
         </section>
       </div>
       <div className="bg-cyan/5">
         <main className="container">
-          <h4 className="text-center pt-10">What Shezlong offers you</h4>
+          <h4 className="text-center pt-10">{t('What_Shezlong_offers_you')}</h4>
           <div className="w-[100px] h-[2px] mx-auto mt-2 mb-10 bg-cyan " />
           <section className="lg:grid lg:grid-cols-3 lg:gap-8">
             {data?.map((el) => {
               return (
-                <article key={Math.random()} className="flex bg-white rounded-sm gap-4 mb-5">
+                <article key={Math.random()} className="flex bg-white rounded-sm mb-5">
                   <img src={el?.image_path} className="max-h-[120px]" alt="" />
-                  <aside className="py-5">
+                  <aside className="py-5 px-3 font-[500] text-[16px] ">
                     <h5 className="mb-5">{el?.header}</h5>
                     <p>{el?.body}</p>
                   </aside>
