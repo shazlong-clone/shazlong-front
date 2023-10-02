411bba7410179035c4483d52988a699048d8a8f9
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-10-02 15:15:24 +0300

    solve remote

--- src/config/enviroment.config.js
+++ src/config/enviroment.config.js
@@ -1,7 +1,8 @@
 // Config
 export const ENTRY_ROUTE = '/sign-up';
 export const TOKEN_PAYLOAD_KEY = 'authorization';
-export const PROD_API = '';
+// eslint-disable-next-line no-undef
+export const PROD_API = process.env.REACT_APP_PROD_BASE_API;
 // eslint-disable-next-line no-undef
 export const DEV_API = process.env.REACT_APP_DEV_BASE_API;
 // eslint-disable-next-line no-undef
