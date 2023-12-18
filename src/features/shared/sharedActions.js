d442ddf0284d63b2b475f5f136865ef9327cdc3f
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-12-18 22:34:49 +0200

    change public

--- src/features/shared/sharedActions.js
+++ src/features/shared/sharedActions.js
@@ -1,21 +1,22 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import service from '../../config/userService';
+import axios from 'axios';
 
 export const getCountries = createAsyncThunk('getCountries', async () => {
-  const res = await service.get('/data/countries.json');
+  const res = await axios.get('/data/countries.json');
   return res?.data;
 });
 export const getLangs = createAsyncThunk('getLangs', async () => {
-  const res = await service.get('/data/lang.json');
+  const res = await axios.get('/data/lang.json');
   return res?.data;
 });
 export const getSpecialization = createAsyncThunk('getSpecialization', async () => {
-  const res = await service.get('/data/specialty.json');
+  const res = await axios.get('/data/specialty.json');
   return res?.data;
 });
 
 export const getPrefix = createAsyncThunk('getPrefix', async () => {
-  const res = await service.get('/data/prefix.json');
+  const res = await axios.get('/data/prefix.json');
   return res?.data;
 });
 
