fc7d2bb6dd1a72248c400cac2660b9d0e93fe5dd
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2024-05-25 18:11:43 +0300

    fix chat styles

--- src/components/Shared/CustomerService.js
+++ src/components/Shared/CustomerService.js
@@ -15,7 +15,6 @@ import { RxCross2 } from 'react-icons/rx';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeChat } from '../../features/theme/themeSlice';
 import shazlong from '../../assets/images/shazlong.png';
-import { Link } from 'react-router-dom';
 import EmojiDropdown from './EmojiDropdown';
 import { useMediaQuery } from '@uidotdev/usehooks';
 import therapist from '../../assets/images/therapist.webp';
@@ -52,96 +51,104 @@ function CustomerService({ close, isMobile = false }) {
   }, [textAreaRef, message]);
   const { isChatOpen } = useSelector((state) => state?.theme);
   const ref = useRef();
+  const handelClose = () => {
+    if (isMobile) {
+      removeClass(ref.current, 'animate__backInUp');
+      addClass(ref.current, 'animate__backOutDown');
+      setTimeout(() => {
+        dispatch(closeChat());
+      }, [500]);
+    } else {
+      dispatch(closeChat());
+    }
+  };
+  useEffect(() => {
+    addEventListener(
+      'load',
+      function () {
+        window.scrollTo(1, 0);
+      },
+      false,
+    );
+  }, []);
   return (
     <>
       <div
         ref={ref}
         className={clsx(
-          'fixed top-0 left-0 h-[100vh] overflow-hidden w-full bg-[var(--rs-bg-card)] z-[51] lg:static lg:h-[80vh] lg:w-[400px] lg:rounded-md animate__animated animate__faster',
+          'fixed top-0 left-0 h-[100dvh] duration-100 overflow-hidden w-full bg-[var(--rs-bg-card)] z-[51] lg:static lg:h-[80vh] lg:w-[400px] lg:rounded-md animate__animated animate__faster',
           isChatOpen && isMobile && 'animate__backInUp',
         )}
       >
-        <aside className={clsx(activeTabe === 1 ? 'block' : 'hidden')}>
-          <section className="text-white bg-[var(--rs-primary-700)] px-5 p-10 lg:rounded-t-md xl:py-5">
-            <article
-              onClick={() => {
-                if (isMobile) {
-                  removeClass(ref.current, 'animate__backInUp');
-                  addClass(ref.current, 'animate__backOutDown');
-                  setTimeout(() => {
-                    dispatch(closeChat());
-                  }, [500]);
-                } else {
-                  dispatch(closeChat());
-                }
-              }}
-              className="flex justify-between items-center"
-            >
-              <img width="50px" height="50px" src={logo_white} alt="intercomcdn" />
-              <span onClick={close} className="cursor-pointer">
-                <IconButton style={{ padding: '10px' }} className="bg-transparent" appearance="primary">
-                  <CgClose className="text-[20px]" />
-                </IconButton>
-              </span>
-            </article>
-            <article className="mt-5 xl:mt-10 mb-4">
-              <AvatarGroup stack>
-                <Avatar circle key={cs1} src={cs1} alt="cs1" />
-                <Avatar circle key={cs2} src={cs2} alt="cs2" />
-                <Avatar circle key={cs3} src={cs3} alt="cs3" />
-              </AvatarGroup>
-            </article>
-            <article>
-              <h3 className="text-white/60">
-                {t('Hi')} 👋 {i18n.resolvedLanguage === 'ar' ? user?.fullArName : user?.fullEnName}
-              </h3>
-              <h3>{t('How_We_Can_Help_You')}</h3>
-            </article>
-          </section>
-          <section className="px-5 pt-1 pb-5 bg-gradient-to-b from-[var(--rs-primary-700)] to-white">
-            <Card className="text-gray rounded-[10px] py-4">
-              <aside className="flex justify-between items-center">
-                <div>
-                  <strong className="text-[16px] font-extrabold">{t('Send_Us_A_Message')}</strong>
-                  <p className="text-gray/60 text-[14px]">{t('we_typically_reply_in_under_a_minute')}</p>
-                </div>
-                <div className="flex items-center text-xl cursor-pointer rtl:rotate-180">
-                  <IconButton onClick={() => setActiveTabe(2)} appearance="link" style={{ padding: '10px' }}>
-                    <IoMdSend className="text-[20px]" />
+        {activeTabe === 1 && (
+          <aside className={clsx(activeTabe === 1 ? 'block animate__animated animate__fadeIn' : 'hidden')}>
+            <section className="text-white bg-[var(--rs-primary-700)] px-5 p-10 lg:rounded-t-md xl:py-5">
+              <article onClick={handelClose} className="flex justify-between items-center">
+                <img width="50px" height="50px" src={logo_white} alt="intercomcdn" />
+                <span onClick={close} className="cursor-pointer">
+                  <IconButton style={{ padding: '10px' }} className="bg-transparent" appearance="primary">
+                    <CgClose className="text-[20px]" />
                   </IconButton>
-                </div>
+                </span>
+              </article>
+              <article className="mt-5 xl:mt-10 mb-4">
+                <AvatarGroup stack>
+                  <Avatar circle key={cs1} src={cs1} alt="cs1" />
+                  <Avatar circle key={cs2} src={cs2} alt="cs2" />
+                  <Avatar circle key={cs3} src={cs3} alt="cs3" />
+                </AvatarGroup>
+              </article>
+              <article>
+                <h3 className="text-white/60">
+                  {t('Hi')} 👋 {i18n.resolvedLanguage === 'ar' ? user?.fullArName : user?.fullEnName}
+                </h3>
+                <h3>{t('How_We_Can_Help_You')}</h3>
+              </article>
+            </section>
+            <section className="px-5 pt-1 pb-5 bg-gradient-to-b from-[var(--rs-primary-700)] to-white">
+              <Card className="text-gray rounded-[10px] py-4">
+                <aside className="flex justify-between items-center">
+                  <div>
+                    <strong className="text-[16px] font-extrabold">{t('Send_Us_A_Message')}</strong>
+                    <p className="text-gray/60 text-[14px]">{t('we_typically_reply_in_under_a_minute')}</p>
+                  </div>
+                  <div className="flex items-center text-xl cursor-pointer rtl:rotate-180">
+                    <IconButton onClick={() => setActiveTabe(2)} appearance="link" style={{ padding: '10px' }}>
+                      <IoMdSend className="text-[20px]" />
+                    </IconButton>
+                  </div>
+                </aside>
+              </Card>
+            </section>
+            <section className="fixed bottom-0 shadow-[#00000024_0px_0px_25px] w-full border-t border-b-0 border-r-0 border-l-0  border-solid border-[var(--rs-gray-100)] flex text-3xl">
+              <aside
+                onClick={() => setActiveTabe(1)}
+                className="grow grid cursor-pointer text-center text-cyan text-[var(--rs-primary-700)] hover:bg-[var(--rs-btn-subtle-hover-bg)]  active:bg-[var(--rs-btn-subtle-hover-bg)]  duration-300 p-6"
+              >
+                <span className=" flex justify-center h-[30px]">
+                  <AiFillHome />
+                </span>
+                <strong className="text-sm">{t('Home')}</strong>
               </aside>
-            </Card>
-          </section>
-          <section className="absolute bottom-0 shadow-[#00000024_0px_0px_25px] w-full border-t border-b-0 border-r-0 border-l-0  border-solid border-[var(--rs-gray-100)] flex text-3xl">
-            <aside
-              onClick={() => setActiveTabe(1)}
-              className="grow grid cursor-pointer text-center text-cyan text-[var(--rs-primary-700)] hover:bg-[var(--rs-btn-subtle-hover-bg)]  active:bg-[var(--rs-btn-subtle-hover-bg)]  duration-300 p-6"
-            >
-              <span className=" flex justify-center h-[30px]">
-                <AiFillHome />
-              </span>
-              <strong className="text-sm">{t('Home')}</strong>
-            </aside>
-            <aside
-              className="grow grid cursor-pointer text-center active:text-[var(--rs-primary-400)] hover:bg-[var(--rs-btn-subtle-hover-bg)]  active:bg-[var(--rs-btn-subtle-hover-bg)]  duration-300 p-6"
-              onClick={() => setActiveTabe(2)}
-            >
-              <span className="flex justify-center h-[30px]">
-                <Badge content={1}>
-                  <BiMessageAltDetail />
-                </Badge>
-              </span>
-              <strong className="text-sm">{t('Messages')}</strong>
-            </aside>
-          </section>
-        </aside>
+              <aside
+                className="grow grid cursor-pointer text-center active:text-[var(--rs-primary-400)] hover:bg-[var(--rs-btn-subtle-hover-bg)]  active:bg-[var(--rs-btn-subtle-hover-bg)]  duration-300 p-6"
+                onClick={() => setActiveTabe(2)}
+              >
+                <span className="flex justify-center h-[30px]">
+                  <Badge content={1}>
+                    <BiMessageAltDetail />
+                  </Badge>
+                </span>
+                <strong className="text-sm">{t('Messages')}</strong>
+              </aside>
+            </section>
+          </aside>
+        )}
+
         {activeTabe && (
           <aside
             className={clsx(
-              activeTabe === 2
-                ? 'grid grid-rows-[auto_1fr_auto] h-full animate__animated animate__fadeInBottomRight animate__faster'
-                : 'hidden',
+              activeTabe === 2 ? 'grid grid-rows-[auto_1fr_auto] h-full animate__animated animate__fadeIn' : 'hidden',
             )}
           >
             <section className="bg-[var(--rs-primary-700)] p-5 text-white lg:rounded-t-md">
@@ -152,7 +159,7 @@ function CustomerService({ close, isMobile = false }) {
                   </IconButton>
                 </span>
                 <h4 className="pt-2">Customer Suport</h4>
-                <span className="cursor-pointer flex items-center" onClick={() => dispatch(closeChat())}>
+                <span className="cursor-pointer flex items-center" onClick={handelClose}>
                   <IconButton onClick={close} style={{ padding: '10px' }} className="bg-transparent" appearance="primary">
                     <RxCross2 className="text-[20px]" />
                   </IconButton>
@@ -183,7 +190,7 @@ function CustomerService({ close, isMobile = false }) {
                 </span>
               </div>
             </section>
-            <section className={clsx('shadow-[0px_-17px_30px_0px_rgba(0,0,0,0.1)] pb-[50]', lg && 'mb-0')}>
+            <section className={clsx('shadow-[0px_-17px_30px_0px_rgba(0,0,0,0.1)]', lg && 'mb-0')}>
               <article className="text-base flex py-3 items-end px-5 gap-1 bg-[var(--rs-gray-50)] border border-solid border-[var(--rs-gray-100)] border-t-1 border-b-0 border-r-0 border-l-0">
                 <textarea
                   id="review-text"
--- src/components/Shared/CustomerServiceMobile.js
+++ src/components/Shared/CustomerServiceMobile.js
@@ -1,10 +1,18 @@
 import clsx from 'clsx';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import CustomerService from './CustomerService';
 
 function CustomerServiceMobile() {
   const { isChatOpen } = useSelector((state) => state?.theme);
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (isChatOpen) {
+      body.classList.add('overflow-hidden');
+    } else {
+      body.classList.remove('overflow-hidden');
+    }
+  }, [isChatOpen]);
   if (isChatOpen) {
     return (
       <div className={clsx(isChatOpen ? 'block' : 'hidden')}>
